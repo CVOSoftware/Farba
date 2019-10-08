fc5d04d95c943dd885f556d5b0fae812243a380e
cvosoftware <cvosoftware@gmail.com>
2019-10-08 23:44:35 +0300

    Added adaptive size to image viewer items

---
+++ Farba/Resources/Converters/AdaptiveHeightContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Farba.Resources.Converters
+{
+    internal class AdaptiveHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var width = (double)value;
+            return width + width / 3.5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
